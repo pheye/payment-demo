73064cf57c62300eddbb77e4a7e20b6376b0f2c4
LIUWENCAN <pheye@qq.com>
2019-01-29 20:03:41 +0800

    1.支持通过Stripe完成信用卡单次扣款

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,20 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Yansongda\Pay\Pay;
 use Log;
+use Auth;
 
 class HomeController extends Controller
 {
-    protected $config = [
-        'app_id' => '2018051760159810',
-        'notify_url' => 'http://abc.tunnel.papamk.com/onNotify',
-        'return_url' => 'http://abc.tunnel.papamk.com/onReturn',
-        'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjFcrd6p+oy7wfCZBjaUBL8YOkwxCYSnV+M6x/mi37EOpxwkWWQjm50RvSpTNYgoUkPX2HMtwDurvVvcT/YN+RM8mR6uLcNz/s+rMnDMKhgF6tfflkyxBFeIXolcwP0BKHA8LPHAaQzQN6ILPpABOdPflz1KlU/LBOtikZGKf3MH03LOBwQNoAeYNcfjMHM7AFTnTdeIqe9LPZEc9cLBzZgb8xB+oeuZE97kEA6lYgpI73l0Pqjag3Y/LeFjLO1RL1Z0puM/aRV4CHnEG6BHQGVoQtvrVwxdry1R8XYGYWjE2zse+U7DdTHCQKYA4JUcJVLYRd6WY8UM+Z+P1srGakwIDAQAB',
-        'private_key' => 'MIIEpAIBAAKCAQEAwXVJ4AQ7L8OVPU+dG4hSI720Twx4IRKja9w3hpcMfB0ptUVckg2qo9qgI3Irq2eW69ApDwXe101oY2hf9dR/vw3VqoLvTBbFg2ouMCTOmO12eqPGQCf4P2sE2DRAte/Gj9xPgaNfz4kwQaUKF5kz3pLEtSrqegtJ+waVqg/wW3gS9n0TIAyVfkF1JJESTUvxHjyJnkLYc4SE/3yGgPYo1FgIH0c4vH0gt+5/vGNt2NeXqvNTwVZnI+yUq0mFEPrulSl/jxf939lIpG7wM4rXllEScbCm67UaEesUwVKlgLEau1dSGaw5O26CPFW3Q//J2lmxDi/pgZ/GuPLpAkQMdQIDAQABAoIBAQCBGpzqeuhkv8lg2Tinnxnx8C/ccR3aNG4LFTOZBkS2r1+eMWPNtIEkSEqGXTL3eHInfQtpkylb1bTMYmNn0yBNiBYHbMn1pVy6jqm0R1v4gsCGp7UuK35oFCtvfa+Ruypv5z7H7ReLkKo7pMBPb1ZGOvw7SyCfwdjlRUer7mchIp+d6GPhqCa1JueZ+ROOdNVv435K1mHdwUp6G931/wewh8DxYs3s+VrRBapzmAbPHZM1syzxkjMnFF2zr4PIcaO6ilMfJm8CaHtSEQEjCKtKdexN/xxOa61XeYN4e1JM0sng7yRSAJIZ//i65G3O+mVcT6cs0pfnAsEfYB1SlywhAoGBAOT+6pn2CO6q8fiuckcZ1QU4dZX5QFfEK4lc3Rd276yOuzWbXGxnSlJvUVbRoq2vIKutmQhvxezbI2L1epBNIGojo7wvygQp5n//oXfO8wi0Nswy9KPXg2P4RCwnsRwGDL2bQMzb4RG2u0PerwFCgo47ynE2Bn3fsVfL/xm3SJijAoGBANhFicDL6pjvC7NWOj+dPj0dHtsGDbamzoQRSIYFIEzi2+ONrpVcIgsM6Eu/nH4wholao+/KwZIohnxB7elW80+ZSh6409zoVimnNle7NhdaKJBNLw0RPoUN87vGi2suePaZvK05dPhBZgLJLmjo23gUcGrOCXUJBxAu7G40tqAHAoGBAItv9aZePD4n9UM55fgJcL7HDEKQDW/j0abI1w+MqpBmMPSJb5PKqWTcx6iX6fjcZIZlJIQQm3KIVVnSpBjt/cjjfrX+W4KBiQtzuvxbFX29Toi0lmaVujnLWKeSW7P2sxdZS/HyCXBh1lGTbPjVaO321mRtQzKuXSxa2TDte9UFAoGAO+on242ilHPFHg3JXU5Gq8+heLQYoH1dwSC4oshQxnwixsu9jgtUMxOEV1fiFuOCohLGT+wv7Dnl6A6rxnUcOvUQn6o8p3qGREvatjtbJOUJT9DSNCCO3XWcDG1YunzJbifxKVw9l3kmtabegJniE0Y2TqB95jp2Bnm5bl6UKYkCgYBXyP9NcL5iLOaqcEGffnS5vcdn+hgQ7cXX6U5vNjfV21jaucmHcMtt4d9ebQK1dki+d5lumUl9n9VHFlxi345OcvnosnL1BB+d+k7s2STUHSVqWRGPRwEhJXFSN+/nnzvH9baNIaf5ScAo2l+BPJvhEV+FDJDpT+88UhkMrFG08g==',
-        'log' => [ // optional
-            'file' => './alipay.log',
-            'level' => 'debug'
-        ],
-    ];
+    protected $config = [];
 
     /**
      * Create a new controller instance.
@@ -28,6 +19,18 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
+        $this->config = [
+        'app_id' => env('ALIPAY_APPID'),
+        'notify_url' => 'http://abc.tunnel.papamk.com/onNotify',
+        'return_url' => 'http://abc.tunnel.papamk.com/onReturn',
+        'ali_public_key' => env('ALIPAY_PUBLICK_KEY'),
+        'private_key' => env('ALIPAY_PRIVATE_KEY'),
+        'log' => [ // optional
+            'file' => './alipay.log',
+            'level' => 'debug'
+        ],
+    ];
     }
 
     /**
@@ -39,7 +42,8 @@ class HomeController extends Controller
     {
         $plans = \Pheye\Payments\Models\Plan::all();
         $payments = Auth()->user()->payments()->orderBy('created_at', 'desc')->get();
-        return view('home')->with(['plans' => $plans, 'payments' => $payments]);
+        $user = Auth::user();
+        return view('home')->with(['plans' => $plans, 'payments' => $payments, 'user' => $user]);
     }
 
     public function pay()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,16 +13,30 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+<div>
+    <div>
+        当前计划： {{$user->subscription->getPlan()->display_name}}
+    </div>
+    <form action="/pay" method="post">
+    <input type="hidden" name="plan_name" value="vip" /> 
+    <input type="hidden" value="paypal_ec" name="gateway_name"/>
+    <input type="hidden"  value="0" name="onetime"/>
+    <input type="hidden"  value="1" name="upgrade"/>
+    <input class="btn btn-primary" type="submit" value="升级VIP"/>
+    </form>
+</div>
 <ul>
 @foreach ($plans as $plan) 
 <li>{{$plan->display_name}}: {{$plan->amount}} {{$plan->currency}}
-    <form action="/pay" method="post">               
+    <form action="/pay" method="post" id="form_{{$plan->name}}">               
       <input type="text" name="plan_name" value="{{$plan->name}}" /> 
       <input type="text" class="text" value="paypal_ec" name="gateway_name"/>
       <input type="text" class="text" value="0" placeholder="0为循环扣款 1为一次性扣款" name="onetime"/>
       <input type="text" class="text" value="" placeholder="优惠码" name="coupon"/>
+      @if ($plan->name == 'vip')
+        @component('components.credit', ['formid' => "form_" . $plan->name ])
+        @endcomponent
+      @endif
       <input type="submit" value="Submit" class="btn btn-primary"/>
     </form>  
 </li>
@@ -40,9 +54,14 @@
 @foreach ($payments as $payment)
 <tr>
 <td>
-{{$payment->subscription->agreement_id}} 
-@if ($payment->subscription->agreement_id) 
-<span class="text-danger">{{$payment->subscription->status}}</span>
+@if ($payment->subscription)
+    {{$payment->subscription->agreement_id}} 
+    @if ($payment->subscription->agreement_id) 
+    <span class="text-danger">{{$payment->subscription->status}}</span>
+    @endif
+    <span>({{$payment->subscription->plan}})</span>
+@else
+  <span class="text-info">Onetime Payment</span>
 @endif
 </td>
 <td><span class="text-primary">{{$payment->number}}</span> </td>
@@ -51,7 +70,7 @@
 <td>
 @if ($payment->status == 'completed') <form action="/payments/{{$payment->number}}/refund_request" method="POST">{{ method_field('PUT') }}<input type="submit" value="退款" class="btn btn-default" /></form>
 @endif
-@if ($payment->subscription->agreement_id && $payment->subscription->status != 'canceled') 
+@if ($payment->subscription && $payment->subscription->agreement_id && $payment->subscription->status != 'canceled') 
 <form action="/subscription/{{$payment->subscription->id}}/cancel" method="POST"><input type="submit" value="取消订阅" class="btn btn-default" /></form>
 @endif
 </td>
